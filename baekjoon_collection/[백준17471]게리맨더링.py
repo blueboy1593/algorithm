e1a5bd24407526421f2fad99249288db68bc11b5
blueboy1593 <snb0303@naver.com>
2020-02-07 17:56:33 +0900

    게리맨더링 드디어ㅣ..ㅠㅠ

---
+++ baekjoon_collection/[백준17471]게리맨더링.py
@@ -0,0 +1,90 @@
+from itertools import combinations
+# 드디어 했네... 아 시원해 이 답답한걸.
+
+N = int(input())
+ingu = list(map(int, input().split()))
+injub_list = [ [0] * N for _ in range(N) ]
+for i in range(N):
+    data = list(map(int, input().split()))
+    for j in range(1, len(data)):
+        p = data[j]
+        injub_list[i][p - 1] = 1
+        injub_list[p - 1][i] = 1
+# pprint(injub_list)
+
+# 조합을 다 구해봐?
+# 1단계 가능한 모든 조합 경우의 수 구하기.
+comb_list = []
+half_N = N // 2
+# 조합의 뼈대 만들기
+base = []
+for i in range(N):
+    base.append(i)
+
+for k in range(1, half_N + 1):
+    temp = map(list,combinations(base, k))
+    # temp = combinations(base, k)
+    for a in temp:
+        comb_list.append(a)
+# print(comb_list)
+
+# 2단계 각각 조합에 대해 visited 만들구 아 인접리스트부터
+# 인접 리스트 만들기. 쉽게 만들었다.
+# 노트에 적어둔 것대로 이제 순회공연.
+def check_tree(num, arr):
+    stack = []
+    for j in range(N):
+        if injub_list[num][j] == 1 and visited[j] == False:
+            if j in arr:
+                stack.append(j)
+                visited[j] = True
+
+    while stack:
+        sta = stack.pop()
+        for j in range(N):
+            if injub_list[sta][j] == 1 and visited[j] == False:
+                if j in arr:
+                    stack.append(j)
+                    visited[j] = True
+
+min_answer = 99999999
+for comb in comb_list:
+    # 이 방법 은근히 시간 오래 걸릴 것 같기도 하다
+    visited = [False] * N
+    visited2 = [False] * N
+    num1 = comb[0]
+    visited[num1] = True
+    check_tree(num1, comb)
+    # for k in range(N):
+    #     if k in comb:
+    #         if visited[k] == True:
+    #             visited2[k] = True
+    # 돌아가는 로직 뭐냐...
+
+    comb2 = []
+    for i in range(N):
+        if i not in comb:
+            comb2.append(i)
+    visited[comb2[0]] = True
+    check_tree(comb2[0], comb2)
+
+
+    if visited == [True] * N:
+        sum1 = 0
+        sum2 = 0
+        # 여기는 인구 비교 로직!!!
+        for k in range(N):
+            if k in comb:
+                sum1 += ingu[k]
+            else:
+                sum2 += ingu[k]
+        # if abs(sum1 - sum2) == 9:
+        #     print(comb)
+        if abs(sum1 - sum2) < min_answer:
+            min_answer = abs(sum1 - sum2)
+    else:
+        continue
+
+if min_answer == 99999999:
+    min_answer = -1
+print(min_answer)
